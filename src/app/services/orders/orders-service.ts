c548dde01a65653cdf1f479fceb12ae7c79ca2e7
proav2323 <108835201+proav2323@users.noreply.github.com>
2026-07-01 11:42:51 +0530

    order-filters

--- src/app/services/orders/orders-service.ts
+++ src/app/services/orders/orders-service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Service, signal, WritableSignal } from '@angular/core';
 import { API_URL } from '../../../imp';
 import { order } from '../../../models/order';
+import { Toaster } from '../../services/toaster';
 
 @Service()
 export class OrdersService {
@@ -11,6 +12,7 @@ export class OrdersService {
   API_URL = API_URL;
   token = localStorage.getItem('token');
   orders: WritableSignal<order[]> = signal([]);
+  toastService = inject(Toaster);
 
   getAllOrders() {
     if (!this.token) {
@@ -35,18 +37,61 @@ export class OrdersService {
     if (!this.token) {
       return;
     }
-    // this.isLoading.set(true);
+    this.isLoading.set(true);
     let newHeaders = new HttpHeaders().set('Authorization', `Bearer ${this.token}`);
-    // this.httpClient.get<order[]>(`${this.API_URL}/orders/`, { headers: newHeaders }).subscribe({
-    //   next: (data) => {
-    //     this.errorOccured.set(null);
-    //     this.orders.set(data);
-    //     this.isLoading.set(false);
-    //   },
-    //   error: (err) => {
-    //     this.errorOccured.set(err.message);
-    //     this.isLoading.set(false);
-    //   },
-    // });
+
+    if (status && !dateStart && !dateEnd) {
+      this.httpClient
+        .get<order[]>(`${this.API_URL}/orders/status?status=${status}`, { headers: newHeaders })
+        .subscribe({
+          next: (data) => {
+            this.errorOccured.set(null);
+            this.orders.set(data);
+            this.isLoading.set(false);
+          },
+          error: (err) => {
+            this.errorOccured.set(err.message);
+            this.toastService.showToast(err.message, 'error');
+            this.isLoading.set(false);
+          },
+        });
+    } else if (!status && dateStart && dateEnd) {
+      this.httpClient
+        .get<
+          order[]
+        >(`${this.API_URL}/orders/date?dateStart=${dateStart}&dateEnd=${dateEnd}`, { headers: newHeaders })
+        .subscribe({
+          next: (data) => {
+            this.errorOccured.set(null);
+            this.orders.set(data);
+            this.isLoading.set(false);
+          },
+          error: (err) => {
+            this.errorOccured.set(err.message);
+            this.toastService.showToast(err.message, 'error');
+            this.isLoading.set(false);
+          },
+        });
+    } else if (status && dateStart && dateEnd) {
+      this.httpClient
+        .get<order[]>(
+          `${this.API_URL}/orders/filter?status=${status}&dateStart=${dateStart}&dateEnd=${dateEnd}`,
+          {
+            headers: newHeaders,
+          },
+        )
+        .subscribe({
+          next: (data) => {
+            this.errorOccured.set(null);
+            this.orders.set(data);
+            this.isLoading.set(false);
+          },
+          error: (err) => {
+            this.errorOccured.set(err.message);
+            this.toastService.showToast(err.message, 'error');
+            this.isLoading.set(false);
+          },
+        });
+    }
   }
 }
